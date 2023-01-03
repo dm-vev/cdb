42ca2a2135f741cf906a378431d9b5c4cc42a4ee
dm-vev <80632449+dm-vev@users.noreply.github.com>
2023-01-03 21:07:54 +0300

    Update main_example.c
    Updated example for new functions

--- examples/main_example.c
+++ examples/main_example.c
@@ -5,12 +5,20 @@ int main(int argc, char** argv) {
   cdb_init_database(); // Initializing database
   cdb_set("hello", "world"); // Set to "hello" cell value: "world"
   cdb_set("foo", "bar"); // Set to "foo" cell value: "bar"
+  cdb_set("delete", "eggs"); // "Deleted" must be deleted
+  cdb_remove("delete"); // Deleting "Deleted"
 
   cdb_save_database("test.db"); // Save database to test.db
   cdb_load_database("test.db"); // Load database to test.db
 
   printf("%s\n", cdb_get("hello")); // Print "hello" cell
   printf("%s\n", cdb_get("foo")); // Print "foo" cell
+
+  char** keys = get_keys(); // Get all keys from database
+  for (int i = 0; i < db.count; i++) {
+    printf("Key: %s\n", keys[i]); // Printing. Key "delete" must not displayed
+  }
+  free(keys); // Free memory
 
   return 0;
 }
